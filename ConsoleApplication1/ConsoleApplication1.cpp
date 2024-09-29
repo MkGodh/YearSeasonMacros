f90ec9c2ad944a4224aae99b2fc3ebc10e205313
MkaGodh <fenryrvla@yandex.ru>
2024-09-29 15:07:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+#define SPRING 1
+#define SUMMER 0
+#define AUTUMN 0
+#define WINTER 0
+
+#if SPRING
+
+int main()
+{
+    std::cout << "Spring" << std::endl;
+}
+
+#endif
+
+#if SUMMER
+
+int main()
+{
+    std::cout << "Summer" << std::endl;
+}
+
+#endif
+
+#if AUTUMN
+
+int main()
+{
+    std::cout << "AUTUMN" << std::endl;
+}
+
+#endif
+
+#if WINTER
+
+int main()
+{
+    std::cout << "WINTER" << std::endl;
+}
+
+#endif
